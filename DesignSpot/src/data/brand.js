dcef5999f2450c1eef289dd2c073bff85ff7a02f
Aser <ajhubero16@gmail.com>
2023-03-05 19:04:52 +0800

    Committed the data for the whole app.
    1. Added the data including 5 items per category. Sofa, Table, Chairs, Cabinets, Mattress. The length of the array is 25.
    
    2. Added the Category for the filtering purposes.
    
    2. Added the Brand for the filtering purposes.

---
+++ DesignSpot/src/data/brand.js
@@ -0,0 +1,6 @@
+const brand = [
+  { id: 0, name: "EcoSaver" },
+  { id: 1, name: "Delightful" },
+  { id: 2, name: "Lilly" },
+  { id: 3, name: "Thuma" },
+];
---
+++ DesignSpot/src/data/category.js
@@ -0,0 +1,7 @@
+const categories = [
+  { id: 0, name: "Sofa" },
+  { id: 1, name: "Mattress" },
+  { id: 2, name: "Cabinet" },
+  { id: 3, name: "Chair" },
+  { id: 4, name: "Table" },
+];
