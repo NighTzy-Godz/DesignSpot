bf86559d2ec6e6a86900fc42c0e3b023e78bac23
Aser <ajhubero16@gmail.com>
2023-03-08 16:00:04 +0800

    Added the Go Back To Products Button for Single Product Page

--- DesignSpot/src/components/feature/Product/component/SingleProduct.jsx
+++ DesignSpot/src/components/feature/Product/component/SingleProduct.jsx
@@ -36,44 +36,46 @@ const SingleProduct = () => {
     } = product;
     if (brand)
       return (
-        <div className="container">
-          <div className="single_product_container">
-            <div className="single_product_left">
-              <LinkButton path="/products" label="Go Back to Products" />
-              <div className="img">
-                <img src={img} alt="" />
+        <div className="single_product">
+          <div className="container">
+            <LinkButton path="/products" label="<= Go Back to All Products" />
+            <div className="single_product_container">
+              <div className="single_product_left">
+                <div className="img">
+                  <img src={img} alt="" />
+                </div>
               </div>
-            </div>
-            <div className="single_product_right">
-              <h1 className="product_name">{name}</h1>
-              <p className="product_price">{price}</p>
-              <p className="product_desc">{desc}</p>
+              <div className="single_product_right">
+                <h1 className="product_name">{name}</h1>
+                <p className="product_price">{price}</p>
+                <p className="product_desc">{desc}</p>
 
-              <div className="product_section">
-                <p className="product_section_title">Availabilty: </p>
-                <p className="product_section_data">{stock}</p>
-              </div>
+                <div className="product_section">
+                  <p className="product_section_title">Availabilty: </p>
+                  <p className="product_section_data">{stock}</p>
+                </div>
 
-              <div className="product_section">
-                <p className="product_section_title">SKU: </p>
-                <p className="product_section_data">{sku}</p>
-              </div>
+                <div className="product_section">
+                  <p className="product_section_title">SKU: </p>
+                  <p className="product_section_data">{sku}</p>
+                </div>
 
-              <div className="product_section">
-                <p className="product_section_title">Brand: </p>
-                <p className="product_section_data">{brand.name}</p>
-              </div>
+                <div className="product_section">
+                  <p className="product_section_title">Brand: </p>
+                  <p className="product_section_data">{brand.name}</p>
+                </div>
 
-              <div className="product_section">
-                <p className="product_section_title">Size: </p>
-                <p className="product_section_data">{size}</p>
-              </div>
+                <div className="product_section">
+                  <p className="product_section_title">Size: </p>
+                  <p className="product_section_data">{size}</p>
+                </div>
 
-              <div className="product_section">
-                <p className="product_section_title">Color: </p>
-                <p className="product_section_data">{color}</p>
+                <div className="product_section">
+                  <p className="product_section_title">Color: </p>
+                  <p className="product_section_data">{color}</p>
+                </div>
+                <CartCounter />
               </div>
-              <CartCounter />
             </div>
           </div>
         </div>
