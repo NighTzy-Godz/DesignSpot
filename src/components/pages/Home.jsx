faf62853b438bc0c5d2cd4b1c41a95eeb3e6976b
Aser <ajhubero16@gmail.com>
2023-03-14 22:06:34 +0800

    Designed and added the home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,74 @@
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import "../../assets/css/home.css";
+import home_mainBg from "../../assets/img/home_mainBg.jpg";
+import ProductCart from "../feature/Product/component/ProductCard";
+import LinkButton from "../ui/button/LinkButton";
+import ReasonBox from "./ReasonBox";
+import useFeatureProduct from "./useFeaturedProduct";
+
+const landing_page_style = {
+  background: `linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url(${home_mainBg})`,
+};
 
 const Home = () => {
   useEffect(() => {
     document.title = "Home";
   }, []);
 
-  return <div className="">I am the home section</div>;
+  const { featuredProduct } = useFeatureProduct();
+
+  const renderFeatureProduct = featuredProduct.map((item) => {
+    return <ProductCart data={item} />;
+  });
+
+  return (
+    <React.Fragment>
+      <div className="landing_page" style={landing_page_style}>
+        <div className="container">
+          <div className="landing_page_text">
+            <h1>
+              the <span>Maeve Sofabed</span>,
+            </h1>
+            <h1>now in performance velvet</h1>
+            <LinkButton path="/products/5" label="Shop Maeve Sofabed" />
+          </div>
+        </div>
+      </div>
+      <div className="featured_product">
+        <div className="container">
+          <div className="featured_product_title">
+            <h1>Featured Products</h1>
+          </div>
+          <div className="featured_product_items">{renderFeatureProduct}</div>
+          <div className="featured_product_btn">
+            <LinkButton path="/products" label="See All Products" />
+          </div>
+        </div>
+      </div>
+
+      <div className="reason">
+        <div className="container">
+          <div className="reason_title">
+            <h1>Reasons to Shop at Us</h1>
+          </div>
+          <div className="reason_items">
+            <ReasonBox
+              icon={<i className="fa-regular fa-face-kiss-wink-heart"></i>}
+              title="Loved by Everyone"
+            />
+            <ReasonBox
+              icon={<i className="fa-solid fa-jet-fighter-up"></i>}
+              title="Fast Delivery"
+            />
+            <ReasonBox
+              icon={<i class="fa-solid fa-tags"></i>}
+              title="Discounted Items"
+            />
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Home;
